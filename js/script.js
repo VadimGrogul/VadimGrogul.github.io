27e0d06b20f4ed9dced6e330b3264a77f00ead40
VadimGrogul <grogulvadim@gmail.com>
2018-03-13 23:01:33 +0200

    js change

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-$(function(){
-    $(".data-categories").not(":eq(1)").hide();
-
-    $('.article-categories').click(function(){
-        $('.article-categories').removeClass('active-category').eq($(this).index()).addClass("active-category");
-
-
-
-       var qwe = $(this).index();
-       console.log(qwe);
-       
-       $('.data-categories').hide().eq($(this).index()).fadeIn();
+$(document).ready(function(){
+        $(".data-categories").not(":eq(1)").hide();
+    
+        $('.article-categories').click(function(){
+            $('.article-categories').removeClass('active-category').eq($(this).index()).addClass("active-category");
+    
+    
+    
+           var qwe = $(this).index();
+           console.log(qwe);
+           
+           $('.data-categories').hide().eq($(this).index()).fadeIn();
     })
 })
 

93e1dc12eaa4544e60ae266dc1ff1055dea45e75
VadimGrogul <grogulvadim@gmail.com>
2018-03-13 22:44:20 +0200

    new JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 $(function(){
-    $('.article-categories').click(function(){
-        $('.article-categories').removeClass('active-category');
-        $(this).addClass('active-category');
+    $(".data-categories").not(":eq(1)").hide();
 
-        var qwe = $('.article-categories').eq(this);
+    $('.article-categories').click(function(){
+        $('.article-categories').removeClass('active-category').eq($(this).index()).addClass("active-category");
+
+
+
+       var qwe = $(this).index();
        console.log(qwe);
+       
+       $('.data-categories').hide().eq($(this).index()).fadeIn();
     })
 })
 
